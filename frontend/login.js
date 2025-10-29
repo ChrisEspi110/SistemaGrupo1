2c0272cad4636ce8ee927c02271977b1995a5eed
ChrisEspi110 <christopherespinal110@gmail.com>
2025-10-29 03:21:51 -0400

    Actualizacion1

--- frontend/login.js
+++ frontend/login.js
@@ -8,22 +8,20 @@ form.addEventListener('submit', async e => {
     const clave = document.getElementById('clave').value;
 
     try {
-const res = await fetch('https://backend-ng4h.onrender.com/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ usuario, clave })
-});
-
+        const res = await fetch('/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({usuario, clave})
+        });
 
         const data = await res.json();
 
-if(data.success){
-    window.location.href = 'https://chrisespi110.github.io/SistemaGrupo1/estudiantes.html';
-} else {
-    mensajeError.textContent = 'Usuario o contraseña incorrectos';
-    mensajeError.style.display = 'block';
-}
-
+        if(data.success){
+            window.location.href = 'estudiantes.html';
+        } else {
+            mensajeError.textContent = 'Usuario o contraseña incorrectos';
+            mensajeError.style.display = 'block';
+        }
     } catch (err) {
         mensajeError.textContent = 'Error de conexión. Intenta nuevamente.';
         mensajeError.style.display = 'block';
